0861dc341c3384da900057d8aa2c0b6104c7ce3a
DaeHun <skypedanny@naver.com>
2019-09-30 18:22:24 +0900

    All I need to know about State

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import Component from './Component';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Component />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current =>({ count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current =>({ count: current.count - 1}));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The Number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
--- movie_app/src/Component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Component() {
-    return(
-        <h3>React is very awesome!</h3>
-    )
-}
-
-export default Component;
